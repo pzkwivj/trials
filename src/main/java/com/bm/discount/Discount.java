90cbfe64534bd6d360c70626ddb218c656a4e604
Admin <Admin@DESKTOP-72KLDSP>
2026-04-29 21:44:49 +0200

    changes in folders

--- src/main/java/com/bm/discount/Discount.java
+++ src/main/java/com/bm/discount/Discount.java
@@ -5,11 +5,13 @@
 package com.bm.discount;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author Admin
  */
+@SpringBootApplication
 public class Discount {
 
     public static void main(String[] args) {

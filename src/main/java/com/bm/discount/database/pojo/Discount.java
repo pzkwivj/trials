2fdf33f2022155ac758fe3cc28d4b68a313bdf5d
Admin <Admin@DESKTOP-72KLDSP>
2026-05-05 21:58:21 +0200

    json ignore

--- src/main/java/com/bm/discount/database/pojo/Discount.java
+++ src/main/java/com/bm/discount/database/pojo/Discount.java
@@ -1,5 +1,7 @@
 package com.bm.discount.database.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
@@ -23,35 +25,71 @@ public class Discount {
     @Column(name = "to_date", nullable = false)
     private LocalDate endDate; // Mapirano na to_date
 
+    @JsonIgnoreProperties("discounts") // Isključi samo listu popusta unutar proizvoda
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
+    @JsonIgnoreProperties("products") // Isključi listu proizvoda unutar kompanije
     @ManyToOne
     @JoinColumn(name = "company_id", nullable = false)
     private Company company;
 
     // --- Getters and Setters ---
     // Obavezno proveri da li imena metoda odgovaraju poljima iznad
-    
-    public Long getDiscountId() { return discountId; }
-    public void setDiscountId(Long discountId) { this.discountId = discountId; }
+    public Long getDiscountId() {
+        return discountId;
+    }
 
-    public Double getPercentage() { return percentage; }
-    public void setPercentage(Double percentage) { this.percentage = percentage; }
+    public void setDiscountId(Long discountId) {
+        this.discountId = discountId;
+    }
 
-    public Double getDiscountedPrice() { return discountedPrice; }
-    public void setDiscountedPrice(Double discountedPrice) { this.discountedPrice = discountedPrice; }
+    public Double getPercentage() {
+        return percentage;
+    }
 
-    public LocalDate getStartDate() { return startDate; }
-    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
 
-    public LocalDate getEndDate() { return endDate; }
-    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
+    public Double getDiscountedPrice() {
+        return discountedPrice;
+    }
 
-    public Product getProduct() { return product; }
-    public void setProduct(Product product) { this.product = product; }
+    public void setDiscountedPrice(Double discountedPrice) {
+        this.discountedPrice = discountedPrice;
+    }
 
-    public Company getCompany() { return company; }
-    public void setCompany(Company company) { this.company = company; }
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
